4b1940ab861590fc7b9e511ce2b7023526f32197
Imran-S-heikh <imransheikhsadi@gmail.com>
2022-01-30 16:27:13 +0600

    Chapter 12.4-Test Complete

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,7 @@ fn main() -> () {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!(
-        "Searching For {}: In ifle {}",
-        config.query, config.filename
-    );
-
+    
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
 
